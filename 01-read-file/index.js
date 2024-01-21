cdc5e01b0d04d38543a365501b9de73be2302ba0
pirnataly <piroznatal@gmail.com>
2024-01-21 23:38:08 +0300

    feat: add solution of first task

---
+++ 01-read-file/index.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.resolve(__dirname, './text.txt');
+const readableStream = fs.createReadStream(filePath,'utf-8');
+readableStream.on('data', chunk => console.log(chunk));
